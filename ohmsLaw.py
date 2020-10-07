c17d5a0b6b5c8cb6e7e6a5bfca1cee984ef5a89f
Aiyana Arnobit <anzarnobit28@gmail.com>
2020-10-07 12:57:02 -0700

    Update ohmsLaw.py

--- ohmsLaw.py
+++ ohmsLaw.py
@@ -1,3 +1,5 @@
+from convertUnits import *
+
 #string -> number
 #write ohms as O
 def ohms_law(voltage=None, current=None, resistance=None):
@@ -27,21 +29,3 @@ def ohms_law(voltage=None, current=None, resistance=None):
         return "resistance: " + str(voltage_amt / current_amt)
     else:
         raise ValueError
-
-#conversion stuff
-def do_conversion(num, value):
-    if value[0] == "m":
-        return mili_to_normal(num)
-    elif value[0] == "k":
-        return kilo_to_normal(num)
-    elif len(value) == 1:
-        return num
-
-def mili_to_normal(num):
-    return num / 1000
-
-def kilo_to_normal(num):
-    return num * 1000
-
-def centi_to_normal(num):
-    return num / 100
