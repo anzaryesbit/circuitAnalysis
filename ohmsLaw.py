0bdf541c0f4f114de31e49cd2eb04c4884ee84d6
Aiyana Arnobit <anzarnobit28@gmail.com>
2020-10-07 12:55:50 -0700

    Update ohmsLaw.py

--- ohmsLaw.py
+++ ohmsLaw.py
@@ -28,11 +28,6 @@ def ohms_law(voltage=None, current=None, resistance=None):
     else:
         raise ValueError
 
-
-def joules_law(voltage=None, current=None, resistance=None, power=None):
-    if resistance is None:
-        return voltage ** 2 / power
-
 #conversion stuff
 def do_conversion(num, value):
     if value[0] == "m":
